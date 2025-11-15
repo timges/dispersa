195a41a222f784429d90570cde3f5c5dd0d5dda2
Tim Gesemann <tgesemann@adobe.com>
2025-11-15 10:45:00 +0100

    chore: add prettier configuration

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+}
+
